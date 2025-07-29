fa0f1a4801ccd45934bc4dfa417164ae94614ba5
D-Arox <boschnermaximilian@gmail.com>
2025-07-29 16:36:09 +0200

    .htaccess

--- config/database.php
+++ config/database.php
@@ -1,3 +1,32 @@
 <?php
+class Database {
+    private $host;
+    private $db_name;
+    private $username;
+    private $password;
+    private $port;
+    public $conn;
 
+    public function __construct() {
+        $this->host = $_ENV['DB_HOST'];
+        $this->db_name = $_ENV['DB_NAME'];
+        $this->username = $_ENV['DB_USER'];
+        $this->password = $_ENV['DB_PASSWORD'];
+        $this->port = $_ENV['DB_PORT'];
+    }
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $exception) {
+            if ($_ENV['DEBUG_MODE'] === 'true') {
+                echo "Connection error: " . $exception->getMessage();
+            }
+        }
+        return $this->conn;
+    }
+}
 ?>
