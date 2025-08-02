bdcad4214bf5a001d05a73cc9102a64786d04dac
D-Arox <boschnermaximilian@gmail.com>
2025-08-02 20:29:20 +0200

    Update head.php

--- includes/components/head.php
+++ includes/components/head.php
@@ -27,10 +27,10 @@
 <meta name="twitter:image" content="https://blufox-studio.com/assets/images/twitter-card.png">
 
 <!-- Favicon -->
-<!-- <link rel="icon" type="image/x-icon" href="/assets/images/favicon.ico">
-<link rel="icon" type="image/png" sizes="32x32" href="/assets/images/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicon-16x16.png">
-<link rel="apple-touch-icon" sizes="180x180" href="/assets/images/apple-touch-icon.png"> -->
+<link rel="icon" type="image/x-icon" href="/assets/images/logo/BluFox_Studio_Logo.svg">
+<link rel="icon" type="image/png" sizes="32x32" href="/assets/images/logo/BluFox_Studio_Logo.svg">
+<link rel="icon" type="image/png" sizes="16x16" href="/assets/images/logo/BluFox_Studio_Logo.svg">
+<link rel="apple-touch-icon" sizes="180x180" href="/assets/images/logo/BluFox_Studio_Logo.svg">
 
 <!-- Preconnect to external domains -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
